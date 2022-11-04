19afde6b36d9f8e29a330566b59f3d0a7bd20f1d
Oleksii Vaskevych <alexeyffi@ukr.net>
2022-11-04 17:37:42 +0200

    ефіл-06.07.08 done

--- js/task-06.js
+++ js/task-06.js
@@ -3,12 +3,19 @@ const inputEl = document.querySelector("#validation-input");
 
 inputEl.addEventListener("blur", onInputLength);
 
+const qwerty = inputEl.getAttribute("data-length");
+console.log("qwerty =", qwerty);
+
+console.log(typeof qwerty);
+
 function onInputLength(event) {
   // console.log("eve ", event.currentTarget.value.length);
 
-  if (event.currentTarget.value.length >= 6) {
+  if (event.currentTarget.value.length === Number(qwerty)) {
     inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
   } else {
     inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
   }
 }
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,31 @@
+const form = document.querySelector(".login-form");
+// console.log("form", form)
 
+form.addEventListener("submit", onSubmit);
+
+let newObj = {};
+
+function onSubmit(event) {
+  event.preventDefault();
+  const emailValue = form.email.name;
+  const passwordlValue = form.password.name;
+
+  const emaiInputValue = form.elements.email.value;
+  const passwordInputValue = form.elements.password.value;
+
+  if (emaiInputValue === "" || passwordInputValue === "") {
+    return console.log("Alert! All fields must be filled");
+  } else {
+    newObj[emailValue] = emaiInputValue;
+    newObj[passwordlValue] = passwordInputValue;
+  }
+
+  console.log(newObj);
+  event.currentTarget.reset();
+}
+
+//      1  Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
+//      2  Під час відправлення форми сторінка не повинна перезавантажуватися.
+// 3  Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
+// 4  Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
+// 5  Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
